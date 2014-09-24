3cb0a269b28ead6990ebd41584331a432273ab8d
8pockets <e.pockets@hotmail.co.jp>
2014-09-25 01:05:00 +0900

    first-commit

---
+++ MaterialButton/MaterialButton.h
@@ -0,0 +1,19 @@
+//
+//  MaterialButton.h
+//  MaterialButton
+//
+//  Created by 8pockets on 2014/09/25.
+//  Copyright (c) 2014年 8pockets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialButton.
+FOUNDATION_EXPORT double MaterialButtonVersionNumber;
+
+//! Project version string for MaterialButton.
+FOUNDATION_EXPORT const unsigned char MaterialButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialButton/PublicHeader.h>
+
+
